ece7a2d600a0b3b6d54b7190c6227680fb29812d
dimespinosa <di.espinosa@udla.edu.co>
2017-12-18 10:03:28 -0500

    primera entrega

---
+++ Entregar/Electoral/build/generated-sources/ap-source-output/ws/Persona_.java
@@ -0,0 +1,14 @@
+package ws;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2017-12-11T23:54:51")
+@StaticMetamodel(Persona.class)
+public class Persona_ { 
+
+    public static volatile SingularAttribute<Persona, String> personaNom;
+    public static volatile SingularAttribute<Persona, String> personaId;
+
+}
---
+++ Entregar/Electoral/src/java/ws/Persona.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Diana
+ */
+@Entity
+@Table(name = "persona")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Persona.findAll", query = "SELECT p FROM Persona p")
+    , @NamedQuery(name = "Persona.findByPersonaId", query = "SELECT p FROM Persona p WHERE p.personaId = :personaId")
+    , @NamedQuery(name = "Persona.findByPersonaNom", query = "SELECT p FROM Persona p WHERE p.personaNom = :personaNom")})
+public class Persona implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "PersonaId")
+    private String personaId;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "PersonaNom")
+    private String personaNom;
+
+    public Persona() {
+    }
+
+    public Persona(String personaId) {
+        this.personaId = personaId;
+    }
+
+    public Persona(String personaId, String personaNom) {
+        this.personaId = personaId;
+        this.personaNom = personaNom;
+    }
+
+    public String getPersonaId() {
+        return personaId;
+    }
+
+    public void setPersonaId(String personaId) {
+        this.personaId = personaId;
+    }
+
+    public String getPersonaNom() {
+        return personaNom;
+    }
+
+    public void setPersonaNom(String personaNom) {
+        this.personaNom = personaNom;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (personaId != null ? personaId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Persona)) {
+            return false;
+        }
+        Persona other = (Persona) object;
+        if ((this.personaId == null && other.personaId != null) || (this.personaId != null && !this.personaId.equals(other.personaId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ws.Persona[ personaId=" + personaId + " ]";
+    }
+    
+}
